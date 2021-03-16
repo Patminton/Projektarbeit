3842d82fdc24ee2dce42742f37928a1e291b4619
Patminton <s8scpatr@stud.uni-saarland.de>
2021-03-16 14:13:29 +0100

    1 Versuche mit asyncio

--- NebenläufigkeitÜbungen/excercise.py
+++ NebenläufigkeitÜbungen/excercise.py
@@ -1,1 +1,16 @@
+import asyncio
+import time
 
+
+async def sleeper():
+    print("vor dem Warten ", time.strftime("%d.%m.%Y %H:%M:%S"))
+    await asyncio.sleep(2)
+    print("nach dem Warten ", time.strftime("%d.%m.%Y %H:%M:%S"))
+    return
+
+async def main():
+    await asyncio.gather(sleeper(), sleeper(),sleeper())
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
