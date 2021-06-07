ca9db70922cd9e3a81b02582036d1fb5845f9ae1
Patminton <s8scpatr@stud.uni-saarland.de>
2021-06-07 22:06:40 +0200

    hardcode 2 Basis fertig, Threading in versuch 1 nach wie vor fehlerhaft

--- Factory/hardcodetry1.py
+++ Factory/hardcodetry1.py
@@ -17,71 +17,63 @@ class MyThread(threading.Thread):
     def run(self):
         lockMe.acquire()
         print("Starte ", self.name)
+        MyThread.m1("A")
+        MyThread.m1("A")
+        MyThread.m1("B")
+        MyThread.m1("C")
+        print("Zwischenzeit: {}".format(time.time() - startablauf))
+        MyThread.m2("A")
+        MyThread.m3("B")
+        MyThread.m3("C")
+        MyThread.Zusammenbauen([a, b, c])
         print("Beende ", self.name)
         lockMe.release()
 
-
-def m1(token, thread):
-    starttime = time.time()
-    lockMe.acquire()
-    cap.pop(0)
-    print("input: " + token + " output: " + token)
-    time.sleep(10)
-    cap.insert(0, "CAP")
-    lockMe.release()
-    print('Dauer: {}'.format(time.time()-starttime))
-    return(token)
-
-
-def m2(token, thread):
-    starttime = time.time()
-    lockMe.acquire()
-    cap2 = 0
-    print("input: " + token + " output: " + token)
-    if token == "A":
-        time.sleep(30)
-    else:
-        time.sleep(15)
-    cap2 = 1
-    lockMe.release()
-    print("Dauer: {}".format(time.time()-starttime))
-    return(token)
-
-
-def m3(token, thread):
-    starttime = time.time()
-    lockMe.acquire()
-    cap3 = 0
-    print("input: " + token + " output: " + token)
-    if token == "A" or token == "B":
+    def m1(token):
+        starttime = time.time()
+        cap.pop(0)
+        print("input: " + token + " output: " + token)
         time.sleep(10)
-    else:
-        time.sleep(25)
-    cap3 = 1
-    lockMe.release()
-    print("Dauer: {}".format(time.time()-starttime))
-    return(token)
+        cap.insert(0, "CAP")
+        print('Dauer: {}'.format(time.time() - starttime))
+        return (token)
 
-def Zusammenbauen(tokens, thread):
-    starttime = time.time()
-    lockMe.acquire()
-    print("input: " + str(tokens) + " output: abc")
-    prod = ["abc"]
-    lockMe.release()
-    print("Dauer: {}".format(time.time() - starttime))
-    print("Gesamtablaufdauer: {}".format(time.time() - startablauf))
-    return(prod)
+    def m2(token):
+        starttime = time.time()
+        cap2 = 0
+        print("input: " + token + " output: " + token)
+        if token == "A":
+            time.sleep(30)
+        else:
+            time.sleep(15)
+        cap2 = 1
+        print("Dauer: {}".format(time.time() - starttime))
+        return (token)
+
+    def m3(token):
+        starttime = time.time()
+        cap3 = 0
+        print("input: " + token + " output: " + token)
+        if token == "A" or token == "B":
+            time.sleep(10)
+        else:
+            time.sleep(25)
+        cap3 = 1
+        print("Dauer: {}".format(time.time() - starttime))
+        return (token)
+
+    def Zusammenbauen(tokens):
+        starttime = time.time()
+        print("input: " + str(tokens) + " output: abc")
+        prod = ["abc"]
+        print("Dauer: {}".format(time.time() - starttime))
+        print("Gesamtablaufdauer: {}".format(time.time() - startablauf))
+        return (prod)
 
 t1 = MyThread("t1")
 t2 = MyThread("t2")
 
 t1.start()
 t2.start()
-
-a = m1("A", t1)
-b = m1("B", t2)
-c = m1("C", t2)
-a = m2(a,t1)
-b = m3(b, t2)
-c = m3(c, t2)
-abc = Zusammenbauen([a, b, c],t1)
+t1.join()
+t2.join()
